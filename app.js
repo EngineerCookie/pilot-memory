3bc8de58783a2a93a581e4e988fb0d73f48f8acf
Tony <lam_273@hotmail.com>
2022-12-28 14:54:38 -0500

    git init

---
+++ app.js
@@ -0,0 +1,6 @@
+//TEXT TO SPEECH FOR ATC MODE
+/*
+let atc = 'COPA one two three, maintain 150 knots, climb and maintain one three thousand feet altimeter setting two niner niner five, heading three six zero. Contact approach one two two point six. Vee Oh R frequency one one seven point one. '
+let msg = new SpeechSynthesisUtterance(atc);
+window.speechSynthesis.speak(msg)
+*/
